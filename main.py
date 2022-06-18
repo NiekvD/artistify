0744666b268f42560198eac00db0f09944204663
Niek van Delft <guest12085@gmail.com>
2022-06-18 13:18:49 +0200

    mask testing

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from PIL import Image
-img = Image.new('RGB', (500, 500), 'cyan')
-img.save('main.png')
-img.show()
+import cv2
+
+img = cv2.imread('testimg.jpg')  
+rawMask = cv2.imread('mask.jpg')
+
+rawMask = cv2.resize(rawMask, (img.shape[1], img.shape[0]))
+grayImage = cv2.cvtColor(rawMask, cv2.COLOR_BGR2GRAY)  
+(thresh, mask) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
+mask = cv2.bitwise_not(mask)
+res = cv2.bitwise_and(img,img,mask = mask)
+
+cv2.imshow('mask', mask)
+cv2.imshow('res', res)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
